e0d0b0ea52ab33ee323017756de6776b1af8198c
SiddharthaMukherji <siddhartha.mukherji55@gmail.com>
2024-08-23 13:07:42 +0530

    Update setup.py

--- BCG_Sid_Final/setup.py
+++ BCG_Sid_Final/setup.py
@@ -3,8 +3,8 @@ __author__ = "siddhartha"
 from setuptools import find_packages, setup
 
 setup(
-    name="bcg",
-    version="0.0.1",
+    name="BCG_Case_Study_Car_Crash",
+    version="2024.8",
     packages=find_packages(),
     zip_safe=False,
 )
